d74a026c8746417f0d1b22b2a7d145773d25d59b
chrispm164 <chrispm164@gmail.com>
2019-02-02 19:28:47 -0500

    d

---
+++ app/src/main/java/com/example/pennapps/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.pennapps;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+public class HomeFragment extends Fragment {
+    private ProgressBar bar;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
