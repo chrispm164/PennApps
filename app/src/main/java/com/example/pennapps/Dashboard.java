85cb307266cf30577909b41a330358a950065ba8
KhanhNguyen99hn <44149581+KhanhNguyen99hn@users.noreply.github.com>
2019-02-03 00:53:31 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/pennapps/Dashboard.java
@@ -0,0 +1,59 @@
+package com.example.pennapps;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Dashboard extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+
+    private boolean loadFragment(Fragment f) {
+        if (f != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, f).commit();
+            return true;
+        }
+        return false;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new SavingsFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new HistoryFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(new HomeFragment());
+    }
+
+}
